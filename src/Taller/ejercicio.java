08b8e7b65e2723a0b2c1ca0c6f692c1e64279653
Jose-Urtubia <jose.urtubia@alumnos.ucn.cl>
2026-03-29 21:28:48 -0300

    Taller1
    Actualización

---
+++ src/Taller/ejercicio.java
@@ -0,0 +1,49 @@
+package Taller;
+
+import java.util.Scanner;
+
+public class ejercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner s = new Scanner(System.in);
+        boolean cargado = false; 
+
+        int opcion = 0;
+
+        System.out.println("");
+        do {
+            System.out.println();
+            System.out.println("1) Menu de usuario");
+            System.out.println("2) Menu de Analisis");
+     
+            System.out.println("3) Salir");
+            System.out.print("> ");
+
+            String entrada = s.nextLine();
+            System.out.println();
+
+            try {
+                opcion = Integer.parseInt(entrada);
+                menuUsuario(opcion);
+
+            } catch (Exception e) {
+                System.out.println("Por favor ingrese solamente números.");
+                opcion = 0;
+            }
+            
+        }while (opcion !=3);
+        }
+	
+		public static void menuUsuario(int n) {
+			String usuario;
+			Scanner s = new Scanner(System.in);
+			if (n==1){
+				System.out.println("Usuario: ");
+				usuario= s.next();
+				
+			}
+			
+		}
+	}
+
